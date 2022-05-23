43bef44d468c407885faed8fd245e8ef3fcb39f8
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-23 15:33:44 +0200

    make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, quae. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Ab, ad animi ipsam nulla suscipit velit. Alias animi fugiat magnam voluptas.
+    </p>
+</article>
+<a href="/">Go Back</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('post', function () {
+    return view('post');
 });
-
-Route::get('/string', function () {
-    return 'I\'m a string from routes.';
-});
-
-Route::get('/json', function () {
-    return ['foo' => 'bar'];
-});
-
