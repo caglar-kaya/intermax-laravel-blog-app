367dd5393bd9b5718c35be94c93ea2308101a185
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-25 10:55:38 +0200

    blade layouts first option

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-<article>
-    <h1>{{ $post->title }}</h1>
-    <div>
-        {!! $post->body !!}
-    </div>
-</article>
-<a href="/">Go Back</a>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    <article>
+        <h1>{{ $post->title }}</h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+    <a href="/">Go Back</a>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-@foreach ($posts as $post)
-    <article class="{{ $loop->even ? 'foobar' : '' }}">
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
-@endforeach
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+@endsection
