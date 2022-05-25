22cfa43ce7176493095a5060547b2890101414ce
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-25 09:39:47 +0200

    collection sorting and caching refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,18 +32,21 @@ class Post
         $this->body = $body;
         $this->slug = $slug;
     }
-    
+
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ));
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
