f4282cd36727592fcba7a7124db317696776e6ed
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-25 09:03:38 +0200

    find a composer package for post metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,24 +2,52 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
-    public static function all(): array {
-        $files = File::files(resource_path("posts"));
+    public $title;
 
-        return array_map(fn($file) => $file->getContents(), $files);
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    /**
+     * @param $title
+     * @param $excerpt
+     * @param $date
+     * @param $body
+     * @param $slug
+     */
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+    
+    public static function all()
+    {
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
     }
 
-    public static function find($slug) {
-        $path = resource_path("posts/{$slug}.html");
-
-        if (!file_exists($path)) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
+    public static function find($slug)
+    {
+        return static::all()->firstWhere('slug', $slug);
     }
 }
