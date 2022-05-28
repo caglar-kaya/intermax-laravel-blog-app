c2be0c31f37813af4fe525f0c9a568b6a4ad7082
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-28 21:54:50 +0200

    feat(section 4): turbo boost with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,55 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' =>'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'slug' => 'my-family-post',
-            'title' => 'My Family Post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad animi ipsam nulla suscipit velit. Alias animi fugiat magnam voluptas.</p>',
-            'published_at' => now()
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'slug' => 'my-work-post',
-            'title' => 'My Work Post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad animi ipsam nulla suscipit velit. Alias animi fugiat magnam voluptas.</p>',
-            'published_at' => now()
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'slug' => 'my-personal-post',
-            'title' => 'My Personal Post',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad animi ipsam nulla suscipit velit. Alias animi fugiat magnam voluptas.</p>',
-            'published_at' => now()
-        ]);
     }
 }
