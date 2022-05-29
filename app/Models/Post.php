9602a0ae12fad10d8b1b9d376c19866e817716c7
Caglar Kaya <C.kaya@Intermax.nl>
2022-05-29 17:49:35 +0200

    feat(section 4): eager load relationships on an existing model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,11 +11,15 @@ class Post extends Model
 
     protected $guarded = [];
 
-    public function category() {
+    protected $with = ['category', 'author'];
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author() {
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 }
