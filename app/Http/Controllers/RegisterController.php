ba74e6c288d4edd06bea005f724bc148607fc3ad
Caglar Kaya <C.kaya@Intermax.nl>
2022-06-03 13:18:02 +0200

    feat(section 9): show a success flash message

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,7 +13,7 @@ class RegisterController extends Controller
 
     public function store() {
         $attributes = request()->validate([
-            'name' => ['required', 'min:10', 'max:255'],
+            'name' => ['required', 'min:6', 'max:255'],
             'username' => ['required', 'min:7', 'max:255', Rule::unique('users', 'username')],
             'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
             'password' => ['required', 'min:8', 'max:255'],
@@ -21,6 +21,6 @@ class RegisterController extends Controller
 
         User::create($attributes);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
