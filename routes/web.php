cddf64ac5298a099bcb5ea53c17c41d9646ae355
Caglar Kaya <C.kaya@Intermax.nl>
2022-06-07 14:38:27 +0200

    feat(section 11): mailchimp API tinkering

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,18 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us8'
+    ]);
+
+    $response = $mailchimp->lists->getList('750bce79a1');
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
